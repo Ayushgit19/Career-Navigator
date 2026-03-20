feece8b3efa9cd2dbac934147e176245d06343a8
Ayushgit19 <146693918+Ayushgit19@users.noreply.github.com>
2026-03-20 16:58:32 +0530

    Fixed cors

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,16 @@ import express from "express";
 import cors from "cors";
 import aiRoutes from "./routes/aiRoutes.js";
 
-
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://career-navigator-frontend-eight.vercel.app"
+  ],
+  methods: ["GET", "POST"],
+}));
+
 app.use(express.json());
 
 app.use("/api", aiRoutes);
@@ -15,9 +21,5 @@ app.use("/api", aiRoutes);
 app.get("/", (req, res) => {
   res.send("API running");
 });
-
-// app.listen(5000, () => {
-//   console.log("Server running on 5000");
-// });
 
 export default app;
